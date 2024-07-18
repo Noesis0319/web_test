9bfdc570df34d51aef17e5f7d96d0514adb50d3d
Noesis <136559082+Noesis0319@users.noreply.github.com>
2024-07-18 10:30:08 +0900

    a

---
+++ lib/page2.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'main.dart';
+
+class Page2 extends StatefulWidget {
+  @override
+  State createState() => Page2State();
+}
+
+class Page2State extends State<Page2> {
+  @override
+  Widget build(BuildContext context) {
+    return ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+        child: SingleChildScrollView(
+          child: Column(
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MyHomePage(
+                            title: 'asd',
+                          ),
+                        ));
+                  },
+                  child: Text("Move")),
+              Text("Hidden Scroll"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+              Text("Row1"),
+            ],
+          ),
+        ));
+  }
+}
